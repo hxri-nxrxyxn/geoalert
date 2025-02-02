08e9182853ef8d0685f89cd3b2514c2fa4e17f4f
Hari Narayan <harinarayanmr@aol.com>
2025-02-02 16:28:05 +0530

    almost finished

--- src/Stats.svelte
+++ src/Stats.svelte
@@ -4,6 +4,12 @@
     import HomeIcon from "./assets/house-chimney.svg";
     import Sensor from "./assets/sensor-on.svg";
 
+    let accelerometerX = 0;
+    let accelerometerY = 0;
+    let accelerometerZ = 0;
+    let pressure = 0;
+    let altitude = 0; // No relative pressure in this data
+
     import mqtt from "mqtt";
     const client = mqtt.connect("mqtt://test.mosquitto.org:8080");
 
@@ -17,7 +23,19 @@
     });
 
     client.on("message", (topic, message) => {
-        console.log(message.toString());
+        try {
+            const data = JSON.parse(message.toString());
+            console.log("Received data:", data);
+
+            accelerometerX = data.accel_x || 0; // Use accel_x, accel_y, accel_z
+            accelerometerY = data.accel_y || 0;
+            accelerometerZ = data.accel_z || 0;
+            pressure = data.pressure || 0;
+            altitude = data.altitude || 0; // Get altitude
+        } catch (error) {
+            console.error("Error parsing JSON:", error);
+            console.error("Raw message:", message.toString());
+        }
     });
 </script>
 
@@ -30,13 +48,13 @@
             </div>
             <div class="stat__tiles">
                 <div class="stat__tile">
-                    <h3>0.11</h3>
+                    <h3>{accelerometerX.toFixed(2)}</h3>
                 </div>
                 <div class="stat__tile">
-                    <h3>0.11</h3>
+                    <h3>{accelerometerY.toFixed(2)}</h3>
                 </div>
                 <div class="stat__tile">
-                    <h3>0.11</h3>
+                    <h3>{accelerometerZ.toFixed(2)}</h3>
                 </div>
             </div>
         </div>
@@ -46,17 +64,7 @@
             </div>
             <div class="stat__tiles">
                 <div class="stat__tile">
-                    <h3>0.11</h3>
-                </div>
-            </div>
-        </div>
-        <div class="stat">
-            <div class="stat__title">
-                <h1>Relative Pressure <span>mb</span></h1>
-            </div>
-            <div class="stat__tiles">
-                <div class="stat__tile">
-                    <h3>0.11</h3>
+                    <h3>{pressure.toFixed(2)}</h3>
                 </div>
             </div>
         </div>
@@ -66,12 +74,11 @@
             </div>
             <div class="stat__tiles">
                 <div class="stat__tile">
-                    <h3>0.11</h3>
+                    <h3>{altitude.toFixed(2)}</h3>
                 </div>
             </div>
         </div>
     </div>
-
     <div class="foot">
         <Link to="/">
             <img src={HomeIcon} class="home" alt="sensor" />
