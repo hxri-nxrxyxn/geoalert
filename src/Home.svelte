fb55b9d610d4ea2eef2204a4f620b9eceb391f5d
Hari Narayan <harinarayanmr@aol.com>
2025-02-02 18:26:24 +0530

    almost complete

--- src/Home.svelte
+++ src/Home.svelte
@@ -3,16 +3,48 @@
     import HomeIcon from "./assets/house-chimney.svg";
     import Sensor from "./assets/sensor-on.svg";
     import { Link } from "svelte-routing";
+
+    let vibration = "NO";
+    let state = $state("safe");
+
+    import mqtt from "mqtt";
+    const client = mqtt.connect("mqtt://test.mosquitto.org:8080");
+
+    client.on("connect", () => {
+        console.log("Connected to MQTT broker");
+        client.subscribe("geo_alert/sensors", (err) => {
+            if (!err) {
+                console.log("Subscribed to geo_alert/sensors");
+            }
+        });
+    });
+
+    client.on("message", (topic, message) => {
+        try {
+            const data = JSON.parse(message.toString());
+            vibration = data.vibration || "NO";
+            if (vibration == "YES") {
+                state = "unsafe";
+                document.querySelector(".status__text").style.borderColor =
+                    "#d63031";
+            }
+        } catch (error) {
+            console.error("Error parsing JSON:", error);
+            console.error("Raw message:", message.toString());
+        }
+    });
 </script>
 
 <main>
     <Nav />
     <div class="status">
         <div class="status__text">
-            <h1>SAFE</h1>
+            <h1>{state}</h1>
         </div>
     </div>
-    <p>No landslide has been reported from <b>Kozhikode</b></p>
+    <p>
+        {state == "unsafe" ? `` : `No`} Landslide has been <b>reported</b>
+    </p>
 </main>
 <div class="foot">
     <img src={HomeIcon} class="super home" alt="sensor" />
@@ -38,6 +70,7 @@
     }
     .status__text h1 {
         font-size: 1rem;
+        text-transform: uppercase;
     }
     p {
         font-size: 0.75rem;
