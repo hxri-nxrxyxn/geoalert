ea5dfd4259b0f34155f9ffab9bbf65ae78ada59d
Hari Narayan <harinarayanmr@aol.com>
2025-02-02 11:58:34 +0530

    initialised svelte and started on home

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { Router, Route } from "svelte-routing";
+    import Home from "./Home.svelte";
+    import Stats from "./Stats.svelte";
+</script>
+
+<main>
+    <Router>
+        <Route to="/" component={Home} />
+        <Route to="/stats" component={Stats} />
+    </Router>
+</main>
+
+<style></style>
